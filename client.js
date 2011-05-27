7dfbeadd4988e9845b73e7f04030bd4e0422fbe6
Alan Pearson <alanp@sonzea.com>
2011-05-27 09:25:55 -0700

    yup

---
+++ client.js
@@ -0,0 +1,43 @@
+var CLIENT = (function () {
+    console.log("creating client");
+    
+    
+    var socket;
+    
+    function open() {
+        console.log("Opening socket");
+        socket = new WebSocket('ws://localhost:8124/sockettest');
+        socket.onopen = function () {
+            console.log("Socket openened");
+            setTimeout(function() {
+                console.log("sending data");
+                socket.send("hello");
+            }, 1000);
+        };
+        socket.onmessage = function () {
+            console.log("received socket message");
+        };
+        socket.onclose = function () {
+            console.log("Socket closed");
+        };
+        socket.onerror = function () {
+            console.log("Socket error");
+        };
+    }
+    
+    return {
+        open: open
+    };
+}());
+
+
+var UI = (function () {
+    function init() {
+        console.log("UI.init");
+        var el = document.getElementById("openButton");
+        el.addEventListener("click", CLIENT.open, false);
+    }
+
+    console.log("Adding DOMContentLoaded listener");
+    document.addEventListener("DOMContentLoaded",init,false);
+}());
--- server.js
+++ server.js
@@ -2,11 +2,34 @@
 
 var net = require('net');
 
-var server = net.createServer(function (c) {
-  c.write('hello\r\n');
-  c.pipe(c);
+var server = net.createServer(function (client) {
+    var requestHeaders = "";
+    console.log('accepted connection from ' + client.remoteAddress + ':' + client.remotePort);
+    console.log(client);
+    client.on('data', function (data) {
+        console.log('received data: ' + data.toString());
+        requestHeaders = requestHeaders + data.toString();
+        parseRequestHeaders();
+    });
+    client.on('end', function () {
+        console.log('client disconnected');
+    });
+    client.on('error', function (exception) {
+        console.log('exception from client socket');
+        console.log(exception);
+    });
+    
+    function parseRequestHeaders() {
+        var lines = requestHeaders.split('\n');
+        console.log("headers has " + lines.length + " lines");
+        lines.forEach(function (line) {
+            console.log("LINE: " + line.trim());
+        });
+    }
 });
 
-var port = process.env.C9_PORT || 8124;
+var port = process.env.C9_PORT || 8124,
+	host = '0.0.0.0';
 
-server.listen(port, '0.0.0.0');
+server.listen(port, host);
+console.log('listening at ' + host + ':' + port);
